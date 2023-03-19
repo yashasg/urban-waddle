f769eb9764c089b0e123b132601f3ff9e3df9066
yashasg <yashas.gujjar@gmail.com>
2023-03-18 20:08:41 -0700

    got trigger events working and made some improvements to the asteroidSpawnerSystem

--- Game/Assets/Scripts/Systems/AsteroidSpawnerSystem.cs
+++ Game/Assets/Scripts/Systems/AsteroidSpawnerSystem.cs
@@ -4,35 +4,76 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Transforms;
 
-
 namespace Sandbox.Asteroids
 {
     public partial class AsteroidSpawnerSystem : SystemBase
     {
+        struct AsteroidSpawnerSystemPlacementJob : IJobFor
+        {
+            public ComponentLookup<LocalToWorld> localtoWorldLookup;
+            public ComponentLookup<LocalTransform> localTransformLookup;
+            public ComponentLookup<Movement> movementLookup;
+
+            [ReadOnly]
+            public NativeArray<Entity> asteroids;
+            [ReadOnly]
+            public float3 playerPos;
+            [ReadOnly]
+            public float spawnerRadius;
+            public void Execute(int index)
+            {
+
+                Entity asteroid = asteroids[index];
+                uint seed = (uint)(asteroid.Index + index + 1) * 0x9F6ABC1;
+                var random = new Random(seed);
+                var dir = math.float3(math.normalizesafe(random.NextFloat2()),0);
+                var pos = float3.zero + (dir * spawnerRadius);
+                var localToWorld = new LocalToWorld
+                {
+                    Value = float4x4.TRS(pos, quaternion.LookRotationSafe(math.forward(), dir), math.float3(1.0f))
+                };
+                localtoWorldLookup[asteroid] = localToWorld;
+
+
+
+                //update transform
+                LocalTransform transform = localTransformLookup[asteroid];
+                transform.Position = pos;
+                localTransformLookup[asteroid] = transform;
+
+
+                //update direction
+                Movement movement = movementLookup[asteroid];
+                float3 asteroidDirection = math.normalizesafe(float3.zero - pos);
+                movement.direction = math.float2(asteroidDirection.x, asteroidDirection.y);
+
+                movementLookup[asteroid] = movement;
+
+            }
+        }
+
+        private EntityQuery asteroidSpawnerQuery;
         private EntityQuery asteroidQuery;
-        private EntityQuery playerQuery;
+        
         protected override void OnCreate()
         {
             base.OnCreate();
-            EntityQuery asteroidQuery = GetEntityQuery(ComponentType.ReadOnly<AsteroidTag>());
-            EntityQuery playerQuery = GetEntityQuery(ComponentType.ReadOnly<PlayerTag>(), ComponentType.ReadOnly<LocalTransform>());
+
+            asteroidQuery = GetEntityQuery(ComponentType.ReadOnly<AsteroidTag>());
+            asteroidSpawnerQuery = GetEntityQuery(ComponentType.ReadOnly<AsteroidSpawner>());
+
 
         }
         protected override void OnUpdate()
         {
             int spawnedAsteroids = asteroidQuery.CalculateEntityCount();
-            NativeArray<Entity> players = playerQuery.ToEntityArray(Allocator.Temp);
 
+            var asteroidSpawners = asteroidSpawnerQuery.ToComponentDataArray<AsteroidSpawner>(Allocator.Temp);
 
-            var localToWorldComponentLookup = GetComponentLookup<LocalToWorld>();
-            var transformComponentLookup = GetComponentLookup<LocalTransform>();
-            var movementComponentLookup = GetComponentLookup<Movement>();
-
-            var playerPos = localToWorldComponentLookup[players[0]].Position;
-
-            //spawn the large asteroids
-            Entities.ForEach((in AsteroidSpawner spawner) =>
+           //spawn the large asteroids
+            for (int i = 0; i < asteroidSpawners.Length; ++i)
             {
+                AsteroidSpawner spawner = asteroidSpawners[i];
                 //if current session is not complete
                 if (spawnedAsteroids > 0)
                 {
@@ -42,40 +83,23 @@ namespace Sandbox.Asteroids
                 //spawn entities
                 int entitiesToSpawn = spawner.asteroidCountPerSession;
                 NativeArray<Entity> asteroidEntities = CollectionHelper.CreateNativeArray<Entity, RewindableAllocator>(entitiesToSpawn, ref World.Unmanaged.UpdateAllocator);
-                EntityManager.Instantiate(spawner.asteroidBig, asteroidEntities);      //update the position of the spawned entities
-                foreach (var asteroid in asteroidEntities)
+                EntityManager.Instantiate(spawner.asteroidBig, asteroidEntities);
+
+                //update the position of the spawned entities
+                Dependency = new AsteroidSpawnerSystemPlacementJob
                 {
-                    //update local to world
-                    uint seed = (uint)(asteroid.Index + 1) * 0x9F6ABC1;
-                    Random random = new Random(seed);
-                    float3 dir = math.float3(math.normalizesafe(random.NextFloat2()), 0);
-                    float3 pos = dir * spawner.spawnRadius;
+                    localtoWorldLookup = GetComponentLookup<LocalToWorld>(),
+                    localTransformLookup = GetComponentLookup<LocalTransform>(),
+                    movementLookup = GetComponentLookup<Movement>(),
+                    asteroids = asteroidEntities,
+                    playerPos = float3.zero, //TODO: if we want to target asteroid towards the player
+                    spawnerRadius = spawner.spawnRadius
 
-                    LocalToWorld localToWorld = new LocalToWorld
-                    {
-                        Value = float4x4.TRS(pos, quaternion.identity, math.float3(1.0f))
-                    };
-
-                    localToWorldComponentLookup[asteroid] = localToWorld;
-
-                    //update transform
-                    LocalTransform transform = transformComponentLookup[asteroid];
-                    transform.Position = pos;
-                    transformComponentLookup[asteroid] = transform;
+                }.Schedule(entitiesToSpawn, Dependency);
 
 
-                    //update direction
-                    Movement movement = movementComponentLookup[asteroid];
-                    float3 asteroidDirection = math.normalizesafe(playerPos - pos);
-                    movement.direction = math.float2(asteroidDirection.x, asteroidDirection.y);
-
-                    movementComponentLookup[asteroid] = movement;
-
-
-                }
-
-
-            }).WithStructuralChanges().Run();
+                Dependency.Complete();
+            }
 
         }
     }
